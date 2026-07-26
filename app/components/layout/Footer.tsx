601b6228edb925bfcec81661fd0672ded9391c8c
frioxo900-cpu <frioxo900@gmail.com>
2026-07-26 17:52:16 +0530

    Create Footer.tsx

---
+++ app/components/layout/Footer.tsx
@@ -0,0 +1,103 @@
+export default function Footer() {
+
+return(
+
+<footer className="bg-slate-900 text-white mt-20">
+
+<div className="max-w-7xl mx-auto grid md:grid-cols-4 gap-10 px-6 py-16">
+
+<div>
+
+<h2 className="text-2xl font-bold">
+
+Nexablu Trading LLP
+
+</h2>
+
+<p className="mt-5 text-slate-300">
+
+Complete HVAC Supplies & Solutions
+
+</p>
+
+</div>
+
+<div>
+
+<h3 className="font-semibold mb-5">
+
+Products
+
+</h3>
+
+<ul className="space-y-2">
+
+<li>Air Conditioners</li>
+
+<li>Copper Pipes</li>
+
+<li>Ducting</li>
+
+<li>Refrigerant Gas</li>
+
+</ul>
+
+</div>
+
+<div>
+
+<h3 className="font-semibold mb-5">
+
+Contact
+
+</h3>
+
+<p>7977709400</p>
+
+<p>8928167041</p>
+
+<p className="mt-4">
+
+Kurla West
+
+Mumbai
+
+</p>
+
+</div>
+
+<div>
+
+<h3 className="font-semibold mb-5">
+
+Business Hours
+
+</h3>
+
+<p>
+
+Monday - Saturday
+
+</p>
+
+<p>
+
+9:30 AM - 7:30 PM
+
+</p>
+
+</div>
+
+</div>
+
+<div className="border-t border-slate-700 text-center py-5">
+
+© 2026 Nexablu Trading LLP
+
+</div>
+
+</footer>
+
+)
+
+}
