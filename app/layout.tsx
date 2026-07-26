c58a7c37a857ec934f724d8e4c4eddb234f8e588
frioxo900-cpu <frioxo900@gmail.com>
2026-07-26 16:47:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,20 @@
+import "./globals.css";
+import { ReactNode } from "react";
 
+export const metadata = {
+  title: "Nexablu Trading LLP",
+  description:
+    "Wholesale & Retail HVAC Supplies in Mumbai",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
