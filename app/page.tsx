ab12e3955fb13469fe388740d89fa983893384cd
frioxo900-cpu <frioxo900@gmail.com>
2026-07-26 16:47:32 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,36 @@
+export default function Home() {
+  return (
+    <main className="hero">
 
+      <div className="container">
+
+        <h1>
+
+          Complete HVAC Solutions
+          Under One Roof
+
+        </h1>
+
+        <p>
+
+          Wholesale & Retail Supplier of
+
+          Air Conditioners,
+          Copper Pipes,
+          Ducting Material,
+          Refrigerant Gas,
+          HVAC Accessories.
+
+        </p>
+
+        <button>
+
+          Explore Products
+
+        </button>
+
+      </div>
+
+    </main>
+  );
+}
